420594d920cbdbfe1326be96489e70be659ff3ee
Mohammad Runna <mohammadrunnasheikh@gmail.com>
2024-06-09 00:27:10 +0530

    g

--- middleware.js
+++ middleware.js
@@ -1,20 +1,8 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default function customClerkMiddleware(req, res, next) {
-  // Exclude specific routes from the Clerk middleware
-  const excludedRoutes = ['/api/webhook'];
-
-  if (excludedRoutes.some(route => req.url.startsWith(route))) {
-    return next(); // Skip Clerk middleware for excluded routes
-  }
-
-  return clerkMiddleware()(req, res, next);
-}
+// Make sure that the `/api/webhooks/(.*)` route is not protected here
+export default clerkMiddleware()
 
 export const config = {
-  matcher: [
-    '/((?!.*\\..*|_next).*)',
-    '/',
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
